73a19149d4e7795a50215e94093aebdb2367ddcc
Tobias Augspurger <ly0@protonmail.com>
2021-02-18 18:14:20 +0100

    added first API calls

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
+from urllib.parse import urljoin
+import posixpath
 
 
 def main():
     RAAS_API_KEY = os.environ['RAAS_API_KEY']
+    development_API = "https://api-dev.digitalhumani.com/"
+    production_API = "https://api.digitalhumani.com/"
+    enterprise_ID = "cd7cedcd"
+    build_URL = posixpath.join(development_API,"enterprise",enterprise_ID)
+    print(build_URL)
+    r = requests.get(build_URL)
+    my_output = f"{r}"
 
-    my_output = f"Hello {RAAS_API_KEY}"
-
-    print(f"::set-output name=myOutput::{my_output}")
-
+    print(f"::set-output name=myOutput::{r}")
 
 if __name__ == "__main__":
     main()
