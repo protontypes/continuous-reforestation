d5e81a1131ba8d7fd9f65883e3aaf1e1671b7975
Tjark Döring <github@tjark-doering.de>
2021-02-18 19:07:39 +0100

    Added tjark work in progress script to the workflow

--- main.py
+++ main.py
@@ -3,19 +3,43 @@ import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
 from urllib.parse import urljoin
 import posixpath
+import json
 
 
 def main():
-    RAAS_API_KEY = os.environ['RAAS_API_KEY']
-    development_API = "https://api-dev.digitalhumani.com/"
-    production_API = "https://api.digitalhumani.com/"
-    enterprise_ID = "cd7cedcd"
-    build_URL = posixpath.join(development_API,"enterprise",enterprise_ID)
-    print(build_URL)
-    r = requests.get(build_URL)
-    my_output = f"{r}"
+    #RAAS_API_KEY = os.environ['RAAS_API_KEY']
+    #my_output = f"{message}
+    
+    # header maybe
+    # key: x-api-key
+    # value: YOUR_API_KEY
+    
+    company_id = 'cd7cedcd'
+    user = 'tjark'
+    body_para = {
+            "treeCount": 1,
+            "enterpriseId": company_id,
+            "projectId": "93322249",
+            "user": user
+            }
+    url = 'https://api-dev.digitalhumani.com/tree' # currently the dev API
+    
+    # run the post request
+    r = requests.post(url, json=body_para)
+    
+    # put the response int a dictionary for easier handling later
+    ddd = json.loads(r.text)
+    
+    # "later" handling
+    message = ''
+    if r.status_code == requests.codes.ok :
+        message = 'Request went through. All good.\n'+str(ddd['user'])+' planted '+str(ddd['treeCount'])+' tree(s). Very nice.'
+    else :
+        message = 'Something went wrong. \n Your error code is: '+str(r.status_code)
+    
+    print(message)
 
-    print(f"::set-output name=myOutput::{r}")
+    print(f"::set-output name=myOutput::{message}")
 
 if __name__ == "__main__":
     main()
